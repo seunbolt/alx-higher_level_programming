c8ed2d9b0819450c272be0a3d605885ade24cc1c
Oluwaseun <seunbolt@gmail.com>
2023-11-07 13:17:10 -0800

    number divisible by 2

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    j = my_list[0]
+    for i in my_list:
+        if i > j:
+           j = i
+    return j
