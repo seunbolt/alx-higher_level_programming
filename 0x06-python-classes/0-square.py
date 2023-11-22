f9bfadbb4e80cb2ef52eeadbf158598ba723d52e
Oluwaseun <seunbolt@gmail.com>
2023-11-22 11:39:52 -0800

    document square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    It can be used to represent a square and perform basic operations.
+"""
+
+
+class Square:
+    """
+    It can be used to represent a square and perform basic operations.
+
+    Attributes:
+        None
+
+    Returns:
+    None
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the Square class.
+
+It represents a square and initializes its size.
+"""
+
+
+class Square:
+    """
+    This is the Square class.
+
+    It represents a square and initializes its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Init new  Square class with d given size.
+    """
+    def __init__(self, size):
+        self.__size = size
