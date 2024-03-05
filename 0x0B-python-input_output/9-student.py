026d41296edb662f273e6bc1ad0499d2ccf95623
Oluwaseun <seunbolt@gmail.com>
2024-03-05 05:35:17 -0800

    correct class student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-Student = __import__('9-student').Student
+"""
+Class Student that defines a student.
+"""
 
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+class Student:
+    """
+    A class representing a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
